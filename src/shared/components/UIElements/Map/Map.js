34daec10a1cc10c2d2f1e0c81a0f1e0c53d391c3
Eron Powell <erondpowell@gmail.com>
2021-09-23 12:10:18 -0500

    added logic to handle form-wide state. added note into Map component explaining API key is temporarily removed

--- src/shared/components/UIElements/Map/Map.js
+++ src/shared/components/UIElements/Map/Map.js
@@ -3,7 +3,6 @@ import "./Map.css";
 
 // THIS WILL NOT WORK WITHOUT PUTTING YOUR API KEY INTO THE INDEX.HTML SCRIPT TAG
 
-
 const Map = (props) => {
   const mapRef = useRef();
   const { center, zoom } = props;
@@ -35,13 +34,24 @@ const Map = (props) => {
 
   // useEffect(initMap, [center, zoom]);
 
-  return (
-    <div
-      ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
-    ></div>
-  );
-};
+    return (
+      <React.Fragment>
+        <h3>
+          If the map isnt' working, it is because the maps API key has been
+          removed from the script that fetches map in index.html
+        </h3>
+        <p>
+          Map functionality will return later. In the meantime, you can make
+          your own api key to test on developers.google.com/maps
+        </p>
+        <div
+          ref={mapRef}
+          className={`map ${props.className}`}
+          style={props.style}
+        ></div>
+      </React.Fragment>
+    );
+  };
+
 
 export default Map;
