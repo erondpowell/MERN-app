1e4d1ed8879e163c2d1681d0e1c605504e10e09d
Eron Powell <erondpowell@gmail.com>
2021-10-18 12:36:33 -0500

    updated auth page - refactored to set real login. refactored dummy get users into a real request to get users data

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/Loading/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/Loading/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Ron Jonson",
-      image:
-        "https://images.pexels.com/photos/6784829/pexels-photo-6784829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 3,
-    }
-  ];
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
-};
+  //first param is a func, second is an array of dependencies.
+  //useEffect hook runs whenever some dependency changes.
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsloading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsloading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  }
+
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler}/>
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+
+    </React.Fragment>
+  );};
 
 export default Users;
